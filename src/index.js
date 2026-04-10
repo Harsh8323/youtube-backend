a01a7077aa3fd899d84dcd66b739ba3800f259ef
HrH_DeV <144415418+Harsh8323@users.noreply.github.com>
2026-04-11 03:23:06 +0530

    add cookie-parser and cors dependencies with utils code

--- src/index.js
+++ src/index.js
@@ -1,45 +1,47 @@
 // require("dotenv").config({ path: "./env" });
 
 import dotenv from "dotenv";
+
 import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+.then(() =>{
+  app.listen(process.env.PORT || 8000, () =>{
+    console.log(` server is runing on port ${process.env.PORT}`)
+  })
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+})
 
-/*
-import mongoose from "mongoose";
+.catch((error) => {
+console.log("MongoDB connection faliure !!", error)
+})
 
-import express from "express";
 
-const app = express()(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}` / { DB_Name });
-    app.on("error", (error) => {
-      console.log("ERRR: ", error);
-      throw error;
-    });
 
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR:", error);
-    throw err;
-  }
-})();
-*/
+
+
+
+
+// import mongoose from "mongoose";
+
+// import express from "express";
+
+// const app = express()(async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}` / { DB_Name });
+//     app.on("error", (error) => {
+//       console.log("ERRR: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR:", error);
+//     throw err;
+//   }
+// })();
